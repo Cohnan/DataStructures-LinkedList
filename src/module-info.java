42b22c66be9055a82c472502ad01ed2dec38ab4c
Cohnan <sc.puerto10@uniandes.edu.co>
2019-02-01 12:44:35 -0500

    Creacion de proyecto en eclipse

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author cohnan
+ *
+ */
+module taller2 {
+	exports controller;
+	exports view;
+	exports model.vo;
+	exports model.logic;
+	exports api;
+	exports model.data_structures;
+}
