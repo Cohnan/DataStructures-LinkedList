30ef1625a5b65977484b23caaeeb02c89ea293c6
Cohnan <sc.puerto10@uniandes.edu.co>
2019-02-04 01:40:56 -0500

    Note que VOViolationCode era mi clase Infraction. Implementar MovingViolationManager.java

--- src/model/logic/MovingViolationsManager.java
+++ src/model/logic/MovingViolationsManager.java
@@ -1,28 +1,70 @@
 package model.logic;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.opencsv.CSVReader;
+
 import api.IMovingViolationsManager;
 import model.vo.VOMovingViolations;
 import model.data_structures.LinkedList;
 
 public class MovingViolationsManager implements IMovingViolationsManager {
 
+	private LinkedList<VOMovingViolations> listaInfracciones;
 	
 	public void loadMovingViolations(String movingViolationsFile){
-		// TODO Auto-generated method stub
-		
+		CSVReader reader = null;
+		try {
+			reader = new CSVReader(new FileReader("data/Moving_Violations_Issued_in_January_2018.csv"));
+			listaInfracciones = new LinkedList<VOMovingViolations>();
+			
+			VOMovingViolations infraccion;
+		    for (String[] row : reader) {
+		    	infraccion = new VOMovingViolations(row);
+		    	listaInfracciones.anadir(infraccion);
+		    }
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally{
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
 	}
 
 		
 	@Override
 	public LinkedList <VOMovingViolations> getMovingViolationsByViolationCode (String violationCode) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		LinkedList<VOMovingViolations> lista = new LinkedList<>();
+		
+		for (VOMovingViolations infraccion : listaInfracciones) {
+			if (infraccion.getViolationCode().equals(violationCode))
+				lista.anadir(infraccion);
+		}
+		
+		return lista;
 	}
 
 	@Override
 	public LinkedList <VOMovingViolations> getMovingViolationsByAccident(String accidentIndicator) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		LinkedList<VOMovingViolations> lista = new LinkedList<>();
+		
+		for (VOMovingViolations infraccion : listaInfracciones) {
+			if (infraccion.getAccidentIndicator().equals(accidentIndicator))
+				lista.anadir(infraccion);
+		}
+		
+		return lista;
 	}	
 
 
--- src/model/vo/VOMovingViolations.java
+++ src/model/vo/VOMovingViolations.java
@@ -4,14 +4,50 @@ package model.vo;
  * Representation of a Trip object
  */
 public class VOMovingViolations {
-
+	
+	private int objectId;
+	private String ROW_;
+	private String location;
+	private int ADDRESS_ID;
+	private int STREETSEGID;
+	private float XCOORD;
+	private float YCOORD;
+	private String TICKETTYPE;
+	private int FINEAMT;
+	private int totalPaid;
+	private int PENALTY1;
+	private int PENALTY2;
+	private String accidentIndicator;
+	private String ticketIssueDate;
+	private String VIOLATIONCODE;
+	private String violationDescription;	
+	private String ROW_ID;
+	
+	public VOMovingViolations(String[] row) {
+		objectId = Integer.parseInt(row[0]);
+		ROW_ = row[1];
+		location = row[2];
+		ADDRESS_ID = Integer.parseInt(row[3]);
+		STREETSEGID = Integer.parseInt(row[4]);
+		XCOORD = Float.parseFloat(row[5]);
+		XCOORD = Float.parseFloat(row[6]);
+		TICKETTYPE = row[7];
+		FINEAMT = Integer.parseInt(row[8]);
+		totalPaid = Integer.parseInt(row[9]);
+		PENALTY1 = Integer.parseInt(row[10]);
+		PENALTY2 = Integer.parseInt(row[11]);
+		accidentIndicator = row[12];
+		ticketIssueDate = row[13];
+		VIOLATIONCODE = row[14];
+		violationDescription = row[15];
+		ROW_ID = row[16];
+	}
 	
 	/**
 	 * @return id - Identificador único de la infracción
 	 */
 	public int objectId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return objectId;
 	}	
 	
 	
@@ -19,39 +55,41 @@ public class VOMovingViolations {
 	 * @return location - Dirección en formato de texto.
 	 */
 	public String getLocation() {
-		// TODO Auto-generated method stub
-		return 0;
+		return location;
 	}
 
 	/**
 	 * @return date - Fecha cuando se puso la infracción .
 	 */
 	public String getTicketIssueDate() {
-		// TODO Auto-generated method stub
-		return "";
+		return ticketIssueDate;
 	}
 	
 	/**
 	 * @return totalPaid - Cuanto dinero efectivamente pagó el que recibió la infracción en USD.
 	 */
 	public int getTotalPaid() {
-		// TODO Auto-generated method stub
-		return "";
+		return totalPaid;
 	}
 	
 	/**
 	 * @return accidentIndicator - Si hubo un accidente o no.
 	 */
 	public String  getAccidentIndicator() {
-		// TODO Auto-generated method stub
-		return "";
+		return accidentIndicator;
 	}
-		
+	
+	/**
+	 * @return description - Descripción textual de la infracción.
+	 */
+	public String getViolationCode() {
+		return VIOLATIONCODE;
+	}
+	
 	/**
 	 * @return description - Descripción textual de la infracción.
 	 */
 	public String  getViolationDescription() {
-		// TODO Auto-generated method stub
-		return "";
+		return violationDescription;
 	}
 }
